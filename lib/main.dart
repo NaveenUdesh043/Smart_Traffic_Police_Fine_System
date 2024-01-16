ff703125437b6f7f694b4c1a3f819b515e24db22
NaveenUdesh043 <naveenudesh043@gmail.com>
2024-01-16 22:22:31 +0530

    Remove Unused packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:policefinefinal/pages/ActivatePage.dart';
-import 'package:policefinefinal/pages/AdminHome.dart';
-import 'package:policefinefinal/pages/DriverHomePage.dart';
-import 'package:policefinefinal/pages/DriverProfile.dart';
-import 'package:policefinefinal/pages/FineDetails.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:policefinefinal/pages/AdminLogin.dart';
-import 'package:policefinefinal/pages/Home.dart';
-import 'package:policefinefinal/pages/Law.dart';
-import 'package:policefinefinal/pages/PoliceLogin.dart';
-import 'package:policefinefinal/pages/VerifyPolice.dart';
 import 'package:policefinefinal/pages/Welcome_Page.dart';
 import 'firebase_options.dart';
 
